b2f957c7ba09708e6de1266636cec2f7cd85df94
Ke Ji <1002497318@qq.com>
2023-06-01 23:30:23 +0800

    UPDATE README.md

--- train.py
+++ train.py
@@ -254,6 +254,7 @@ def main():
     ]
 
     # Using different optimizer for prompt parameters and model parameters
+    # use args.lr2 to accelerate the optimization of verbalizer0
     verbalizer = prompt_model.verbalizer
     optimizer_grouped_parameters2 = [
         {'params': verbalizer.group_parameters_1, "lr": args.lr},
